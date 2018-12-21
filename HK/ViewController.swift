fe86543f8194bc200dcd1318873f8b914960d2fd
Antonio Willy Malara <antonio.malara@gmail.com>
2018-12-21 04:02:19 +0100

    Adding a standby view

--- HK/ViewController.swift
+++ HK/ViewController.swift
@@ -69,9 +69,11 @@ extension UIColor {
 
 class ClockController : NSObject {
     
-    @IBOutlet var dayName: LCARSLabel!
-    @IBOutlet var date: LCARSLabel!
-    @IBOutlet var time: LCARSLabel!
+    
+    @IBOutlet var dayName: LCARSLabel?
+    @IBOutlet var date: LCARSLabel?
+    @IBOutlet var clock: LCARSLabel?
+    @IBOutlet var longDate: LCARSLabel?
     
     var dayFormatter = DateFormatter()
     var dateFormatter = DateFormatter()
@@ -91,15 +93,18 @@ class ClockController : NSObject {
     
     @objc func updateClock() {
         let now = Date()
-        dayName.text = dayFormatter.string(from: now)
-        date.text    = dateFormatter.string(from: now)
-        time.text    = clockFormatter.string(from: now)
+        
+        dayName?.text = dayFormatter.string(from: now)
+        date?.text = dateFormatter.string(from: now)
+        clock?.text = clockFormatter.string(from: now)
+        longDate?.text = "\(dayFormatter.string(from: now)) • \(dateFormatter.string(from: now))"
     }
     
 }
 
 protocol ColorControllerDelegateParent : AnyObject {
     func colorControllerDelegate(_ delegate: ColorControllerDelegate, didChangeColor to: HSV)
+    func colorControllerDelegate(_ delegate: ColorControllerDelegate, didChangePower to: Bool)
 }
 
 protocol ColorControllerDelegate : AnyObject {
@@ -113,9 +118,8 @@ class ColorController : NSObject, ColorControllerDelegateParent {
     @IBOutlet var hue:          LCARSGradientSlider!
     @IBOutlet var saturation:   LCARSGradientSlider!
     
-    @IBOutlet var colorPreview: UIView!
-    
     var hsv = HSV.zero
+    var power = false
     
     weak var delegate : ColorControllerDelegate? {
         didSet {
@@ -124,8 +128,6 @@ class ColorController : NSObject, ColorControllerDelegateParent {
     }
     
     func updateGradients(hsv: HSV) {
-//        colorPreview.backgroundColor = UIColor(hsv)
-        
         hue.colors = (0 ..< 10)
             .map { hsv.changing(h: CGFloat($0) / 10, s: nil, v: nil).cgColor }
 
@@ -146,6 +148,10 @@ class ColorController : NSObject, ColorControllerDelegateParent {
         brightness.theValue = to.v
     }
     
+    func colorControllerDelegate(_ delegate: ColorControllerDelegate, didChangePower to: Bool) {
+        power = false
+    }
+
     func changeColorFromSlider(h: CGFloat?, s: CGFloat?, v: CGFloat?) {
         delegate?.sendColor(hsv.changing(h: h, s: s, v: v))
     }
@@ -201,22 +207,28 @@ class Debouncer<T> : NSObject {
 class AccessoryColorControllerDelegate : NSObject, HMAccessoryDelegate, ColorControllerDelegate {
     weak var weakParent: ColorControllerDelegateParent?
     
+    var powerCharacteristic      : HMCharacteristic?
     var hueCharacteristic        : HMCharacteristic?
     var brightnessCharacteristic : HMCharacteristic?
     var saturationCharacteristic : HMCharacteristic?
     
+    var onChangePower : (Bool) -> Void = { _ in }
+
     var service : HMService? {
         didSet {
+            powerCharacteristic      = service?.characteristics.first { $0.characteristicType == "00000025-0000-1000-8000-0026BB765291" }
             hueCharacteristic        = service?.characteristics.first { $0.characteristicType == "00000013-0000-1000-8000-0026BB765291" }
             brightnessCharacteristic = service?.characteristics.first { $0.characteristicType == "00000008-0000-1000-8000-0026BB765291" }
             saturationCharacteristic = service?.characteristics.first { $0.characteristicType == "0000002F-0000-1000-8000-0026BB765291" }
             
             service?.accessory?.delegate = self
             
+            powerCharacteristic?.enableNotification(true)      { e in printError(e) }
             hueCharacteristic?.enableNotification(true)        { e in printError(e) }
             brightnessCharacteristic?.enableNotification(true) { e in printError(e) }
             saturationCharacteristic?.enableNotification(true) { e in printError(e) }
             
+            powerCharacteristic?.readValue       { e in self.changePowerFromAccessory(); printError(e) }
             hueCharacteristic?.readValue         { e in self.changeColorFromAccessory(); printError(e) }
             brightnessCharacteristic?.readValue  { e in self.changeColorFromAccessory(); printError(e) }
             saturationCharacteristic?.readValue  { e in self.changeColorFromAccessory(); printError(e) }
@@ -228,6 +240,11 @@ class AccessoryColorControllerDelegate : NSObject, HMAccessoryDelegate, ColorCon
     override init() {
         super.init()
         sendDebouncer.realAction = { [weak self] in self?.realSendColor(hsv: $0) }
+    }
+    
+    func changePowerFromAccessory() {
+        weakParent?.colorControllerDelegate(self, didChangePower: powerCharacteristic?.value as? Bool ?? false)
+        onChangePower(powerCharacteristic?.value as? Bool ?? false)
     }
     
     func changeColorFromAccessory() {
@@ -242,7 +259,12 @@ class AccessoryColorControllerDelegate : NSObject, HMAccessoryDelegate, ColorCon
     }
     
     func accessory(_ accessory: HMAccessory, service: HMService, didUpdateValueFor characteristic: HMCharacteristic) {
-        changeColorFromAccessory()
+        if characteristic.characteristicType == "00000025-0000-1000-8000-0026BB765291" {
+            changePowerFromAccessory()
+        }
+        else {
+            changeColorFromAccessory()
+        }
     }
 
     func sendColor(_ hsv: HSV) {
@@ -266,6 +288,11 @@ class DoubleAccessoryColorControllerDelegate : NSObject, ColorControllerDelegate
     var accessoryDelegate1 = AccessoryColorControllerDelegate()
     var accessoryDelegate2 = AccessoryColorControllerDelegate()
     
+    var onChangePower : (Bool) -> Void = { _ in }
+    
+    var power1 = false
+    var power2 = false
+    
     override init() {
         super.init()
         
@@ -282,6 +309,15 @@ class DoubleAccessoryColorControllerDelegate : NSObject, ColorControllerDelegate
         guard delegate === accessoryDelegate1 else { return }
         weakParent?.colorControllerDelegate(self, didChangeColor: to)
     }
+    
+    func colorControllerDelegate(_ delegate: ColorControllerDelegate, didChangePower to: Bool) {
+        if      delegate === accessoryDelegate1 { power1 = to }
+        else if delegate === accessoryDelegate2 { power2 = to }
+        
+        weakParent?.colorControllerDelegate(self, didChangePower: power1 || power2)
+        onChangePower(power1 || power2)
+    }
+    
 }
 
 class ViewController: UIViewController, HMHomeManagerDelegate {
@@ -291,18 +327,24 @@ class ViewController: UIViewController, HMHomeManagerDelegate {
     let mainColorControllerDelegate   = DoubleAccessoryColorControllerDelegate()
     let accentColorControllerDelegate = AccessoryColorControllerDelegate()
     
-    @IBOutlet weak var clockDay:    LCARSLabel!
-    @IBOutlet weak var clockDate:   LCARSLabel!
-    @IBOutlet weak var clockTime:   LCARSLabel!
-    
+    @IBOutlet var clockController:  ClockController!
     @IBOutlet var colorController:  ColorController!
     @IBOutlet var accentController: ColorController!
     
+    @IBOutlet var standbyView: UIView!
+    
     var lightstripService:   HMService?
-    var bedsideService:      HMService?
     var roadsideService:     HMService?
     var internalsideService: HMService?
 
+    var powerMain = false {
+        didSet { lightsPowerDidChange() }
+    }
+    
+    var powerAccent = false{
+        didSet { lightsPowerDidChange() }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         hm.delegate = self
@@ -311,6 +353,27 @@ class ViewController: UIViewController, HMHomeManagerDelegate {
         
         colorController.delegate  = mainColorControllerDelegate
         accentController.delegate = accentColorControllerDelegate
+        
+        let tapGesture = UITapGestureRecognizer()
+        tapGesture.addTarget(self, action: #selector(standbyViewDidTap))
+        standbyView.addGestureRecognizer(tapGesture)
+        standbyView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(standbyView)
+        
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: standbyView.topAnchor),
+            view.leftAnchor.constraint(equalTo: standbyView.leftAnchor),
+            view.rightAnchor.constraint(equalTo: standbyView.rightAnchor),
+            view.bottomAnchor.constraint(equalTo: standbyView.bottomAnchor),
+        ])
+        
+        mainColorControllerDelegate.onChangePower = {
+            [weak self] in self?.powerMain = $0
+        }
+        
+        accentColorControllerDelegate.onChangePower = {
+            [weak self] in self?.powerAccent = $0
+        }
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -321,7 +384,6 @@ class ViewController: UIViewController, HMHomeManagerDelegate {
         let services = manager.primaryHome?.accessories.flatMap { $0.services } ?? []
         
         lightstripService   = services.first { $0.uniqueIdentifier == lightstripUUID   }
-        bedsideService      = services.first { $0.uniqueIdentifier == bedsideUUID      }
         roadsideService     = services.first { $0.uniqueIdentifier == roadsideUUID     }
         internalsideService = services.first { $0.uniqueIdentifier == internalsideUUID }
 
@@ -331,11 +393,39 @@ class ViewController: UIViewController, HMHomeManagerDelegate {
         accentColorControllerDelegate.service = lightstripService
         
         dump(services.map { "\($0.name) \($0.uniqueIdentifier) \($0.localizedDescription) \($0.serviceType)" })
+        dump(roadsideService?.characteristics.map { "\($0.characteristicType) \($0.metadata!) \($0.description)" })
     }
     
+    @objc func standbyViewDidTap() {
+        hideStandbyView()
+    }
+
+    private func showStandbyView() {
+        UIView.animate(withDuration: 0.3) {
+            self.standbyView.alpha = 1
+        }
+    }
+    
+    private func hideStandbyView() {
+        UIView.animate(withDuration: 0.3) {
+            self.standbyView.alpha = 0
+        }
+    }
+    
+    func lightsPowerDidChange() {
+        let inStandby    = standbyView.alpha == 1
+        let anyLightOn   =  powerAccent ||  powerMain
+        let allLightsOff = !powerAccent && !powerMain
+        
+        if anyLightOn && inStandby {
+            hideStandbyView()
+        }
+        else if allLightsOff && !inStandby {
+            showStandbyView()
+        }
+    }
 }
 
-let lightstripUUID   = UUID(uuidString: "9B04712C-88C9-55C7-BD2B-B831732568A6")!
-let bedsideUUID      = UUID(uuidString: "B64D98A5-B423-5B2A-BC11-690CD7D9E8B5")!
-let roadsideUUID     = UUID(uuidString: "363EFF22-CA03-5ED0-8304-C077CB20D9D8")!
-let internalsideUUID = UUID(uuidString: "885D77EB-4292-5C34-955A-48F261BDB7AD")!
+let lightstripUUID   = UUID(uuidString: "C10389A5-B9BC-53B0-8F24-5FF4685AE82E")!
+let roadsideUUID     = UUID(uuidString: "1AA24C16-346D-5936-BE54-20A6CB0118C3")! // UUID(uuidString: "363EFF22-CA03-5ED0-8304-C077CB20D9D8")!
+let internalsideUUID = UUID(uuidString: "F3FD5A39-513F-52CD-85B2-3FC5D5915826")! // UUID(uuidString: "885D77EB-4292-5C34-955A-48F261BDB7AD")!
